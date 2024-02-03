9dd50c59a38e7a268a22c3f4d535b3d172d78dc2
Cristian Dumbravanu <99369873+CristianDmbr@users.noreply.github.com>
2024-02-03 00:44:14 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,117 +1,65 @@
 import numpy as np 
-import pandas as pd 
-import sklearn.linear_model, sklearn.datasets 
-from sklearn.preprocessing import StandardScaler, MinMaxScaler 
+import pandas as pd
+import sklearn.linear_model, sklearn.datasets
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import matplotlib.pyplot as plt 
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, mean_squared_log_error, max_error, median_absolute_error
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder 
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split 
-from sklearn.impute import SimpleImputer 
+from sklearn.impute import SimpleImputer
+
 pd.options.mode.chained_assignment = None
 
-testData = pd.read_csv('housing_coursework_entire_dataset_23-24.csv') 
+testData = pd.read_csv('housing_coursework_entire_dataset_23-24.csv')
 
-features = ["No.","longitude","latitude","housing_median_age","total_rooms","total_bedrooms","population","households","median_income","ocean_proximity"]
-target = ["median_house_value"]
-
-
+features = ["longitude","latitude","housing_median_age","total_rooms","total_bedrooms","population","households","median_income","ocean_proximity"]
 X_raw = testData[features]
-X_raw = X_raw.drop(columns=['No.'])
+y_raw = testData["median_house_value"]
 
-y_raw = testData[target]
+raw, X_test_raw, y_train, y_test = train_test_split(X_raw, y_raw, test_size=0.20, shuffle=True, random_state=0)
 
-# Replace the missing values of total bedrooms with the mean.
-mean_total_bedrooms = X_raw["total_bedrooms"].mean()
-X_raw["total_bedrooms"].fillna(mean_total_bedrooms, inplace=True)
+# To apply the different imputers, we first have to split our data into seperate numerical and categorical data
+X_train_num = X_raw.select_dtypes(include=np.number)
+X_train_cat = X_raw.select_dtypes(exclude=np.number)
 
-# Filling missing values of ocean proximity with most common category
-most_common_category = X_raw['ocean_proximity'].mode()[0]
-X_raw["ocean_proximity"].fillna(most_common_category,inplace=True)
-
-# Categorical / String feature stats
-print(X_raw.select_dtypes(exclude=np.number).describe())
-
-print(pd.unique(X_raw['ocean_proximity']))
-#print(X_raw.select_dtypes(include=np.number).describe())
-
-# The varible that the model is testing with the target.
-features = ['ocean_proximity']
-X_raw_features = X_raw[features]
-
-# Preprocessing.
-
-# Separates the databse into 80% and 20% for training and testing.
-X_train_raw, X_test_raw, y_train, y_test = train_test_split(X_raw_features, y_raw, train_size=0.80, test_size=0.20, shuffle=True, random_state=0)
-X_train_num = X_train_raw.select_dtypes(include=np.number)
-
-# Create our imputer objects.
+# Create our imputer objects
+# Imputers is what is used for imputing missing values
 numeric_imputer = SimpleImputer(strategy='mean')
+categorical_imputer = SimpleImputer(strategy='most_frequent')
 
-# Fit the imputers on the training data.
+print(sklearn.__version__)
+
+# Fit the imputers on the training data
 numeric_imputer.fit(X_train_num)
+categorical_imputer.fit(X_train_cat)
 
-# Training.
 X_train_num_imp = numeric_imputer.transform(X_train_num)
+X_train_cat_imp = categorical_imputer.transform(X_train_cat)
 
-# Split and transform our test data.
 X_test_num = X_test_raw.select_dtypes(include=np.number)
+X_test_cat = X_test_raw.select_dtypes(exclude=np.number)
 X_test_num_imp = numeric_imputer.transform(X_test_num)
+X_test_cat_imp = categorical_imputer.transform(X_test_cat)
 
-# Feature scalling
-# Scaler Object.
+# Scaler Object
 scaler = MinMaxScaler()
-# Fit on the numeric training data.
+# Fit on the numeric training data
 scaler.fit(X_train_num_imp)
-# Transform the training and test data.
+# Transform the training and test data
 X_train_num_sca = scaler.transform(X_train_num_imp)
 X_test_num_sca = scaler.transform(X_test_num_imp)
 
-X_train = X_train_num_sca
-X_test = X_test_num_sca
+# create the encoder object
+encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+# Fit encoder on teh training data
+encoder.fit(X_train_cat_imp)
+# Transform the test and train data
+X_train_onehot = encoder.transform(X_train_cat_imp)
+X_test_onehot = encoder.transform(X_test_cat_imp)
 
-# Create linear regression object.
-obj = sklearn.linear_model.LinearRegression()
+X_train = np.concatenate([X_train_num_sca, X_train_onehot], axis=1)
+X_test = np.concatenate([X_test_num_sca, X_test_onehot], axis=1)
 
-# Train the model using the training sets.
-obj.fit(X_train, y_train)
-
-# We can make a prediction with the training data
-y_pred_train = obj.predict(X_train)
-# Remember the predictions with the new data give a better indiction of the true model performance.
-# Make predictions using the testing set.
-y_pred = obj.predict(X_test)
-
-# Choosen a single column of the feature matrix so we can plot a 2D scatter plot.
-X_disp = X_test[:,0] 
-
-# Plot outputs
-plt.scatter(X_disp, y_test,  color='black', label='y_test')
-plt.scatter(X_disp, y_pred, color='blue', label='y_pred') 
-plt.xlabel('Ocean Proximity')
-plt.ylabel('Median House Value')
-plt.legend()
-plt.show()
-
-# Regression metrics for both training and testing sets
-mae_train = mean_absolute_error(y_train, y_pred_train)
-rmse_train = np.sqrt(mean_squared_error(y_train, y_pred_train))
-max_err_train = max_error(y_train, y_pred_train)
-medae_train = median_absolute_error(y_train, y_pred_train)
-
-mae_test = mean_absolute_error(y_test, y_pred)
-rmse_test = np.sqrt(mean_squared_error(y_test, y_pred))
-max_err_test = max_error(y_test, y_pred)
-medae_test = median_absolute_error(y_test, y_pred)
-
-# Prints the regression metrics for both training and testing sets
-print('Train - MSE: {:.4f} R2 score: {:.4f}'.format(mean_squared_error(y_train, y_pred_train), r2_score(y_train, y_pred_train)))
-print('Test - MSE: {:.4f} R2 score: {:.4f}'.format(mean_squared_error(y_test, y_pred), r2_score(y_test, y_pred)))
-print('Train - MAE: {:.4f}'.format(mae_train))
-print('Test - MAE: {:.4f}'.format(mae_test))
-print('Train - RMSE: {:.4f}'.format(rmse_train))
-print('Test - RMSE: {:.4f}'.format(rmse_test))
-print('Train - Max Error: {:.4f}'.format(max_err_train))
-print('Test - Max Error: {:.4f}'.format(max_err_test))
-print('Train - Median Absolute Error: {:.4f}'.format(medae_train))
-print('Test - Median Absolute Error: {:.4f}'.format(medae_test))
+# We can see the scaled test results and the OHE category columns now.
+print(X_train)
